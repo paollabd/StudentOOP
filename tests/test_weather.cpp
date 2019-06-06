674a619f64e3941545c2b7a3f97e4f71311eef0d
paollabd <pbd244@nyu.edu>
2019-06-06 13:43:42 -0400

    updated makefile

--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -10,7 +10,7 @@ int main() {
 
     GPS loc = GPS(46.3, 67.2);
     Weather irkutsk = Weather("Irkutsk", loc);
-    irkutsk.set_rating(2);
+    //irkutsk.set_rating(2);
     // next line raises exceptions:
     cout << irkutsk << endl; // .get_name() << endl'
     // to fix this we are going to overload the output operator
